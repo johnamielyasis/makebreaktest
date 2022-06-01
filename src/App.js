d7ea0bb38ba09f0d0572737d7e066b8534eba945
johnamielyasis <johnamielyasis@gmail.com>
2022-05-31 20:32:25 -0700

    adds new game mode option

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ function App() {
         <nav>
           <Link to="/">Classic | </Link>
           <Link to="/temperature">Temperature | </Link>
-          <Link to="/gradient">Gradient</Link>
+          <Link to="/gradient">Gradient |</Link>
+          <Link to="/hidden">Hidden</Link>
         </nav>
         <Routes>
           <Route path="/" element={<Classic />}>Classic</Route>
