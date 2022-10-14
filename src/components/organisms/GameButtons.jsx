cd6b3b371e9679a0bad669967da21fcc2540d5d7
johnamielyasis <johnamielyasis@gmail.com>
2022-10-13 19:59:50 -0700

    fixes alignment for game buttons

--- src/components/organisms/GameButtons.jsx
+++ src/components/organisms/GameButtons.jsx
@@ -1,14 +1,23 @@
+import { Button } from "../atoms/index.js";
+
 const GameButtons = () => {
-    return (
-        <div style={{
-            display: "flex",
-            flex: 2,
-            background: "green",
-            }}
-            > 
-        <p>hw from gamebuttons</p>
-        </div>
-    );
+  return (
+    <div
+      style={{
+        display: "flex",
+        flex: 2,
+        background: "green",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Button>Make</Button>
+      <Button>Break</Button>
+      <Button>Skip</Button>
+    </div>
+  );
 };
 
 export default GameButtons;
