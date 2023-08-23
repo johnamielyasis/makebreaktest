68d57f3b5ba3809f8d78ecdd6a41eaf6842406ab
johnamielyasis <johnamielyasis@gmail.com>
2023-08-22 19:47:59 -0700

    fix: question card margin too big on web version; pushes buttons offscreen

--- src/components/molecules/QuestionCard.jsx
+++ src/components/molecules/QuestionCard.jsx
@@ -24,7 +24,7 @@ const QuestionCard = () => {
       display="flex"
       justifyContent="flex-end"
       alignItems="center"
-      margin="20% 0%"
+      margin="10% 0%"
       padding="24px"
     >
       <StyledTypography
